fd23c39ceda21fa5263ee357d425d4bb1eba1469
c0db1nar10 <51799415+c0db1nar10@users.noreply.github.com>
2020-07-28 10:01:52 -0700

    Update api.py
    comentarios

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
+#RG-Cual es el objetivo? Actualizar los atrasados? Cuales son las 5 que habla en el correo? Cada cuando se corre este proceso?
 @frappe.whitelist()
 def atrasado():
     return frappe.db.sql("UPDATE `sura.totall.mx`.`tabSales Invoice` a left join `sura.totall.mx`.`tabCustomer` b on a.customer = b.customer_name set b.atrasado = DATEDIFF(CURDATE(), a.posting_date), b.factura = a.name where a.status = 'Overdue' or a.status = 'Unpaid'")
+
 
 @frappe.whitelist()
 def bloqueado():
@@ -8,7 +10,8 @@ def bloqueado():
 
 @frappe.whitelist()
 def desbloqueado():
-    return frappe.db.sql("UPDATE `sura.totall.mx`.`tabCustomer` set congelado = 0, credit_limit = 0 where atrasado < 40 and clave like '%%CC%%'")
+    return frappe.db.sql("UPDATE `sura.totall.mx`.`tabCustomer` 
+                         congelado = 0, credit_limit = 0 where atrasado < 40 and clave like '%%CC%%'")
 
 @frappe.whitelist()
 def sinatraso():
